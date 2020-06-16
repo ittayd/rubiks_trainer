0522ddb7c6511488f6624ac8ffed6f23fba4260e
Ittay Dror <idror@akamai.com>
2020-06-16 09:53:07 +0300

    forgot to add the file

---
+++ buttons.js
@@ -0,0 +1,51 @@
+Buttons = (function($){
+    function render_triggers() { 
+        var tbl_body = $('<tbody>');
+        var odd_even = false;
+        $.each(data.triggers, function(k, v) {
+            var tbl_row = $('<tr>').appendTo(tbl_body);						
+            tbl_row.addClass(odd_even ? "odd" : "even");
+            odd_even = !odd_even;               
+
+            $('<td>').append($('<input type="button" class="btn btn-mine btn-sm">').val(k)).appendTo(tbl_row);
+            $('<td>').text(v.moves).appendTo(tbl_row);
+            $('<td>').text(v.inverse || "").appendTo(tbl_row);
+                    
+        });
+        $("#triggers").append(tbl_body);
+    }
+
+    function render_moves(extended = false) {
+        let render_base = (base, lower) => {
+            let a = move => $('<input type="button" class="btn btn-mine btn-sm">').val(move)[0]
+            let row = base => [a(base), a(base + "'"), a(base + "2")]
+            return [row(base).concat(lower ? row(base.toLowerCase()) : [])]
+        }
+        let array = "xyz".split("").flatMap(move => render_base(move, false))
+        if (extended) {
+            array = "ULFRBD".split("").flatMap(move => render_base(move, true)).
+                concat("MES".split("").flatMap(move => render_base(move, false))).
+                concat(array)
+        }
+        array = array[0].map((col, i) => array.map(row => row[i])); // transpose
+        array = array.reduce((tbody, row) => tbody.append(row.reduce((tr, move) => tr.append($('<td>').append(move)), $('<tr>'))), $('<tbody>'))
+        $("#moves").append(array)
+    }
+
+    /** TODO: button to toggle extended move buttons which should also toggle the negative margin (in index.html) so it doesn't go over the cube */
+
+    $(document).ready(_ => {
+        render_moves()
+        render_triggers()
+        $("#triggers").click(e => control.move(algos.parse($(e.target).val()).toMoves()))
+        $("#moves").click(e => {
+            control.move([$(e.target).val()])
+        })	
+    })
+
+    
+    return {
+        render_moves: render_moves,
+        render_triggers: render_triggers
+    }
+})($)
